7640306d8bd89d41f266db006100f025ef411265
bnsmith <bnsmith@5c363a23-4247-0410-ac65-37213380c7e5>
2008-05-14 01:34:15 +0000

    Added another test directory and fixed errors in the test script based on the new files.

--- runtests.py
+++ runtests.py
@@ -40,21 +40,24 @@ def tagEqual(srcFile, destFile, tagType):
 	assert destFile[-4:].lower() == '.mp3'
 	destTagInfo = MP3(destFile)
 	
+	srcTagName = tagNameMap[tagType][0]
+	destTagName = tagNameMap[tagType][1]
+	if srcTagInfo.tags.has_key(srcTagName) == False and destTagInfo.tags.has_key(destTagName) == False:
+		return True
+	
 	# Not sure what's going on with encodings here. It seems like the M4A library
 	# returns unicode strings with the latin-1 encoding, so calling 'encode' with 'UTF-8' returns
 	# a byte string that is unchanged and still in latin-1. When the MP3 library has its tag
 	# object cast to a unicode string, it seems to be encoded in UTF-8, and so needs to be
 	# encoded as latin-1 to be compared with the M4A string. WTF?
-	srcTagName = tagNameMap[tagType][0]
 	srcTag = srcTagInfo.tags[srcTagName]
 	while type(srcTag) == list or type(srcTag) == tuple:
 		srcTag = srcTag[0]
 	if type(srcTag) == int:
 		srcTag = str(srcTag)
 	srcTagStr = srcTag.encode('UTF-8')
-	destTagName = tagNameMap[tagType][1]
 	destTagStr = unicode(destTagInfo.tags[destTagName]).encode('latin-1')
-	if srcTagInfo.tags.has_key(srcTagName) and srcTagStr != destTagStr:
+	if srcTagStr != destTagStr:
 		testerrors.append('Conversion failed: ' + tagType + ' did not match: ' + srcTagStr + ' ' + destTagStr)
 		return False
 	return True
@@ -79,22 +82,36 @@ def ensureConversion(srcDir, destDir, srcType, destType):
 	return True
 
 def main():
+	results = []
+	
 	srcDir1 = u"/home/bnsmith/download/playing/converttomp3_testing/El Ma\xf1ana Test Root"
 	destDir1 = u"/home/bnsmith/download/playing/converttomp3_test_results/El Ma\xf1ana Test Results"
 	if os.path.isdir(destDir1):
 	   shutil.rmtree(destDir1)
 	subprocess.check_call([u"python", u"ConvertToMP3.py", srcDir1, destDir1])
-	test1 = ensureConversion(srcDir1, destDir1, u".m4a", u".mp3")
-	
+	results.append(ensureConversion(srcDir1, destDir1, u".m4a", u".mp3"))
+
+	srcDir2 = u"/home/bnsmith/download/playing/converttomp3_testing/WONKY-FILES_ROOT"
+	destDir2 = u"/home/bnsmith/download/playing/converttomp3_test_results/WONKY-FILES_ROOT_RESULTS"
+	if os.path.isdir(destDir2):
+	   shutil.rmtree(destDir2)
+	subprocess.check_call([u"python", u"ConvertToMP3.py", srcDir2, destDir2])
+	results.append(ensureConversion(srcDir2, destDir2, u".m4a", u".mp3"))
+
+	allSucceeded = True
+	for res in results:
+		if res == False:
+			allSucceeded = False
+
 	print "\n**************************************************************************"
-	if test1 == True:
-		print ("Test succeeded!").encode('UTF-8')
+	if allSucceeded == True:
+		print "Test succeeded!"
 	else:
-		print ("Test failed!").encode('UTF-8')
+		print "Test failed!"
 	print "**************************************************************************"
 	
 	for err in testerrors:
-		print err.encode('UTF-8')
+		print err
 
 if __name__ == '__main__':
 	main()
